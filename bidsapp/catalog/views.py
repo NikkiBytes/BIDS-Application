9f9f4eb9da171f20621d99bf01f79c2193308380
NikkiBytes <nicholletteacosta@gmail.com>
2017-12-04 05:34:26 +0000

    updated, with template directory.

--- bidsapp/catalog/views.py
+++ bidsapp/catalog/views.py
@@ -1,3 +1,8 @@
+from __future__ import unicode_literals
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
 
 # Create your views here.
